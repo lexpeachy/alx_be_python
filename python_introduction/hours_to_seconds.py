6e46277edb7e0b1e973eec5fc5d2318fcd20b4fe
evans kirimi <evanskirimi483@gmail.com>
2024-09-15 14:08:00 +0300

    modified the file

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,6 @@
 hours = 2
 seconds_in_a_minute = 60
 minutes_in_a_hour = 60
-seconds_in_an_hour = seconds_in_a_minute * minutes_in_a_hour
-seconds = seconds_in_an_hour * hours
+seconds_in_an_hour = 3600
+seconds = 3600 * hours
 print(hours, "hour(s) is", seconds, "seconds.")
