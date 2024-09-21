f50d420c54650501739638a8dc57d93888947354
evans kirimi <evanskirimi483@gmail.com>
2024-09-22 00:46:34 +0300

    created a script that prompt the user on the weather and recommend the right cloth to wear

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+# Ask the user to input the current weather from a predefined set of conditions: sunny, rainy, or cold.
+current_weather = (input("what's the weather like today? (sunny/rainy/cold):" ))
+# Based on the user’s input, the program will recommend different types of clothing
+if current_weather == "sunny":
+    print ("Wear a t-shirt and sunglasses.")
+elif current_weather == "rainy":
+    print ("Don't forget your umbrella and a raincoat.")
+elif current_weather == "cold":
+    print ("Make sure to wear a warm coat and a scarf.")
+else:
+    print ("Sorry, I don't have recommendations for this weather.")
