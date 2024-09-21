588a6e814cd66e096970ba5ae64a311672db2cc5
evans kirimi <evanskirimi483@gmail.com>
2024-09-22 01:44:51 +0300

    created a script that calculate operation using match case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+# Ask the user to input two numbers (one at a time)
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+operation = (input("Choose the operation (+, -, *, /): "))
+
+match operation:
+    case "+":
+        result = num1 + num2
+        print (f"the result is", result)
+    case "-":
+        result = num1 - num2
+        print (f"the result is", result)
+    case "*":
+        result = num1 *num2
+        print (f"the ruesult is", result)
+    case "/":
+        if num2 == 0:
+            print (f"cannot devide by zero")
+        else:
+            result = num1 / num2
+            print (f"the result is", result)
+    case _:
+        print (f"invalid operation entered")
