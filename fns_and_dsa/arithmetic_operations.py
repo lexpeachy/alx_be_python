8842c723f903b6c45bdf169a08659cbd6bcd5918
evans kirimi <evanskirimi483@gmail.com>
2024-09-28 18:44:04 +0300

    modified

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,8 +11,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return "zero can not divide a number"
+        else:
+            return num1/num2
         
