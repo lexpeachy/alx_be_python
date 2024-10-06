e0d8e01649511cb9c870e43373746d81268e2bf1
lexpeachy <evanskirimi483@gmail.com>
2024-10-06 21:52:25 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,25 +20,25 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     def setup(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
     def test_addition(self):
-        self.assertEqual(self.calculator.add(10, 5), 15)
-        self.assertEqual(self.calculator.add(-1, 5), 4)
+        self.assertEqual(self.calc.add(10, 5), 15)
+        self.assertEqual(self.calc.add(-1, 5), 4)
 
     def test_subtraction(self):
-        self.assertEqual(self.calculator.subtract(10, 5), 5)
-        self.assertEqual(self.calculator.subtract(-1, 5), -6)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(-1, 5), -6)
     
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(10, 5), 50)
-        self.assertEqual(self.calculator.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
 
     def test_divide(self):
-        self.assertEqual(self.calculator.divide(10, 5), 2)
-        self.assertEqual(self.calculator.divide(-10, 5), -2)
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertEqual(self.calc.divide(-10, 5), -2)
 
         with self.assertRaises(ZeroDivisionError):
-            self.calculator.divide(10, 0)
+            self.calc.divide(10, 0)
 if __name__ == "__main__":
     unittest.main()
 
