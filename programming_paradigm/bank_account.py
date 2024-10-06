36ec9475dfe671071425b8d5c350a04848e79cd6
evans kirimi <evanskirimi483@gmail.com>
2024-10-06 18:53:34 +0300

    modified

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,26 +1,19 @@
-#Define a class named BankAccount.
-#Use the __init__ method to initialize an account_balance attribute. Optionally, accept an initial balance parameter, defaulting to zero.
-class Bankaccount:
-    def __init__(self, initial_balance = 0):
+class BankAccount:
+    def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
 
-# Implement deposit(amount), withdraw(amount), and display_balance() methods.
-# deposit should add the specified amount to account_balance.
-# withdraw should deduct the amount from account_balance if funds are sufficient, returning True; otherwise, return False and do not alter the balance.
-# display_balance should print the current balance in a user-friendly format.
-    def deposit_amount(self, amount):
-        if amount > 0:
-            self.account_balance += amount
-            print(f"Deposited: ${amount}")
-        else:
-            print(f"You can't deposite a negative amount")
-    def withdraw_amount(self, amount):
-        if amount <= self.account_balance:
+    def deposit(self, amount):
+        self.account_balance += amount
+        print(f"Deposited: {amount}")
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount}")
+            print(f"Withdrew: {amount}")
             return True
         else:
-            print(f"Insufficient funds.")
+            print("Insufficient funds")
             return False
+
     def display_balance(self):
-        print(f"Your current account balance is {self.account_balance}")
+        print(f"Current balance: {self.account_balance}")
